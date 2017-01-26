4bdf2a2707ef91e264869e9c17424a558c0b2f07
zoozoz <hyunji9600@naver.com>
2017-01-26 20:50:09 +0900

    인스타 데이터 가져와서 화면에 보여주기
    인스타 데이터 가져와서 화면에 보여주기

--- app/src/main/java/com/yapp/kindpickyeatingandroid/activity/InstagramDetailActivity.java
+++ app/src/main/java/com/yapp/kindpickyeatingandroid/activity/InstagramDetailActivity.java
@@ -1,11 +1,16 @@
 package com.yapp.kindpickyeatingandroid.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.yapp.kindpickyeatingandroid.R;
+import com.yapp.kindpickyeatingandroid.common.KindPickyEatingConstant;
+import com.yapp.kindpickyeatingandroid.dto.InstagramDetailData;
 
 public class InstagramDetailActivity extends AppCompatActivity {
     private ImageView instaPicture;
@@ -20,15 +25,21 @@ public class InstagramDetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_instagram_detail);
 
+        Intent intent = getIntent();
+        InstagramDetailData instagramDetailData = (InstagramDetailData) intent.getSerializableExtra(KindPickyEatingConstant.INSTADETAILDATA);
+        Log.i("ohdoking22",instagramDetailData.getAuthor());
+
         instaPicture = (ImageView)findViewById(R.id.instaPicture);
-        instaPicture.setImageResource(R.drawable.scenery);
+//        instaPicture.setImageResource(R.drawable.scenery);
+        Glide.with(getApplicationContext()).load(instagramDetailData.getMedium()).into(instaPicture);
 
         instaId = (TextView)findViewById(R.id.instaId);
-        instaId.setText("insta.user");
+        instaId.setText(instagramDetailData.getAuthor());
 
         instaApp = (ImageView)findViewById(R.id.instaApp);
         instaApp.setImageResource(R.drawable.bedge);
 
+        
         instaTag = (TextView)findViewById(R.id.instaTag);
         instaTag.setText("#쌀롱딜리셔스 #병아리콩샐러드 #vegan #합정동 #합정맛집 #너무맛있어 #비건 #분위기깡패 #채식주의 #채식");
     }
